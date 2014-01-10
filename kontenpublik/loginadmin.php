bd449c4c233633cd7c5703daf7f12bf177387ef0
Nugieyatalatof <Radit.raha@gmail.com>
2014-01-10 11:29:06 +0700

    fix-kontenpublik:loginadmin

---
+++ kontenpublik/loginadmin.php
@@ -0,0 +1,35 @@
+    <!-- Start: MAIN CONTENT -->
+<div class="page-header">
+            <h2>Administrator Armageddon</h2>
+          </div>
+          
+<center>
+              <table width="439" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td width="439" valign="top">
+                  
+                 
+            <div class="widget-body">
+              <div class="center-align">
+                <form class="form-horizontal form-signin-signup" action="prosesloginadmin.php?access=adminlogin" method="POST">				
+                <input name="username" type="text" id="username" placeholder="Username Administrator">
+                  <input name="password" type="password" id="password" placeholder="Password Administrator">
+                  <div>
+                    <input name="Submit" type="submit" class="btn btn-primary btn-large" value="Access Now">
+                  </div>
+               
+                </form>
+                <br />
+                <br />
+                <br />
+              </div>
+              
+              </div> 
+                
+                  </td>
+                </tr>
+              </table>
+  </center><hr />
+                </div>
+	<!-- End: MAIN CONTENT -->
+    
